618cb263fdff723a910e93eb6d9fe2b0a019bf7b
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-09-23 13:32:01 +0300

    Use a async/await instead of promise

--- requestByUsingFetch.js
+++ requestByUsingFetch.js
@@ -1,6 +1,18 @@
-const getRescountries = (countryCode) => {
+const getRescountries = async (countryCode) => {
+    const res = await fetch('https://restcountries.eu/rest/v2/all', {});
+    if (res.status === 200) {
+        const data = await res.json();
+        return data.find(el => {
+            return el.alpha2Code === countryCode;
+        });
+    } else {
+        throw new Error('Unable to fetch data!!!');
+    }
+}
+
+const getRescountriesOld = (countryCode) => {
     return fetch('https://restcountries.eu/rest/v2/all', {}).then(res => {
-        if(res.status === 200) {
+        if (res.status === 200) {
             return res.json();
         } else {
             throw new Error('Unable to fetch data!!!');
